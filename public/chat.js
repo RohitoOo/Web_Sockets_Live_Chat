6d6018c74c4b734066ce02f8e9a738fe2aef9c2f
Rohit Bhambhani <rohit.m.bhambhani@gmail.com>
2019-04-07 21:53:17 -0400

    Heroku Endpoint Updated

--- public/chat.js
+++ public/chat.js
@@ -1,76 +1,75 @@
 // Connect to Server
-var socket = io.connect('https://pacific-springs-28604.herokuapp.com')
+var socket = io.connect("https://whatsapp-socketio.herokuapp.com")
 
 // Query DOM
 
-var message = document.getElementById('message')
-var handle = document.getElementById('handle')
-var btn = document.getElementById('send')
-var output = document.getElementById('output')
-var feedback = document.getElementById('feedback')
-var joined = document.getElementById('joined')
+var message = document.getElementById("message")
+var handle = document.getElementById("handle")
+var btn = document.getElementById("send")
+var output = document.getElementById("output")
+var feedback = document.getElementById("feedback")
+var joined = document.getElementById("joined")
 
 // Emit Events
 
-$(document).ready(function(){
-
-    $("#form").submit(function(e){
+$(document).ready(function() {
+  $("#form").submit(function(e) {
     e.preventDefault()
 
-        // Send Message From Client To Server
-        socket.emit('chat' , {
-          handle : handle.value,
-          message : message.value
-          })
+    // Send Message From Client To Server
+    socket.emit("chat", {
+      handle: handle.value,
+      message: message.value
+    })
 
-        e.target.message.value = ""
-    });
-});
+    e.target.message.value = ""
+  })
+})
 
-$("#formJoined").submit(function(e){
+$("#formJoined").submit(function(e) {
   e.preventDefault()
 
-$("#form").css({'display' : '' });
-$("#handle").val(e.target.handle.value)
-$("#formJoined").css('display' , 'none');
-$("#feedback").css('display' , '')
+  $("#form").css({ display: "" })
+  $("#handle").val(e.target.handle.value)
+  $("#formJoined").css("display", "none")
+  $("#feedback").css("display", "")
 
-socket.emit('join', {UserName: handle.value });
-
-});
+  socket.emit("join", { UserName: handle.value })
+})
 
 // Event Listener For typing...
 
-message.addEventListener('keypress' , () => {
-  socket.emit('typing' , handle.value )
+message.addEventListener("keypress", () => {
+  socket.emit("typing", handle.value)
 })
 
 // Listen for Output Events ( from Server )
 
-socket.on('chat' , (data) => {
+socket.on("chat", data => {
   feedback.innerHTML = ""
 
-  output.innerHTML += `<p> <strong> ${data.handle} </strong> : ${data.message} </p>`
+  output.innerHTML += `<p> <strong> ${data.handle} </strong> : ${
+    data.message
+  } </p>`
 
-//Scroll
+  //Scroll
 
-$(document).ready(function(){
-
-      $('#chat-window').stop().animate ({
-        scrollTop: $('#chat-window')[0].scrollHeight
-      });
-    });
+  $(document).ready(function() {
+    $("#chat-window")
+      .stop()
+      .animate({
+        scrollTop: $("#chat-window")[0].scrollHeight
+      })
+  })
 })
 
-socket.on('typing' , (data) => {
+socket.on("typing", data => {
   feedback.innerHTML = data + " is typing..."
 })
 
-
-socket.on('join' , (data) => {
-
+socket.on("join", data => {
   joined.innerHTML = data.UserName + " just joined the group chat..."
-  setInterval( ()=> {
+  setInterval(() => {
     joined.innerHTML = ""
-  },2000)
+  }, 2000)
 })
