9fe933e1b47e80e66fadac7488c97d399094c363
Rohit Bhambhani <rohit.m.bhambhani@gmail.com>
2019-04-07 22:15:17 -0400

    Heroku

--- index.js
+++ index.js
@@ -1,44 +1,53 @@
-var express = require('express')
-var socket = require('socket.io')
+var express = require("express")
+var socket = require("socket.io")
 
-var app = express();
+var app = express()
 
 // Init Server
 
 // Heroku Setup (  process.env.PORT  )
-var server = app.listen(process.env.PORT || 3000, (req,res) => {
- console.log('Express Server Is Live On Port 3000')
+var server = app.listen(process.env.PORT || 5000, (req, res) => {
+  console.log("Express Server Is Live On Port 5000")
 })
 
-// Render Html Page From Public Folder
-app.use(express.static('public'))
+// Configuration
+
+app.configure(function() {
+  app.use(express.bodyParser())
+  app.use(express.methodOverride())
+  app.use(app.router)
+  app.use(express.static("public"))
+})
+
+// Heroku won't actually allow us to use WebSockets
+// so we have to setup polling instead.
+// https://devcenter.heroku.com/articles/using-socket-io-with-node-js-on-heroku
+io.configure(function() {
+  io.set("transports", ["xhr-polling"])
+  io.set("polling duration", 10)
+})
 
 // Socket Setup
 var io = socket(server)
 
-
 // Event Handler - Connection to each clinet socket
-io.on('connection' , ( socket ) => {
+io.on("connection", socket => {
+  // User Signed In
+  socket.on("join", data => {
+    io.sockets.emit("join", data)
+  })
 
-// User Signed In
-socket.on('join' , (data) => {
+  // 'chat' is the name of the object being received and sent
 
-  io.sockets.emit('join', data)
-})
+  // (data) is the content of the object being received and sent back
 
-// 'chat' is the name of the object being received and sent
-
-// (data) is the content of the object being received and sent back
-
-  socket.on('chat' , (data) => {
-
+  socket.on("chat", data => {
     // Recieving individual socket's data as an object from front end and
     // emitting / sending to all sockets connected to the server
-    io.sockets.emit('chat' , data)
+    io.sockets.emit("chat", data)
   })
 
-  socket.on('typing' , (data) => {
-    socket.broadcast.emit('typing', data)
+  socket.on("typing", data => {
+    socket.broadcast.emit("typing", data)
   })
-
 })
